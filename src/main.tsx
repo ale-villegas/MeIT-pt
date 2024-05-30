ccf416e16285a6d9797b2608ba2b1d84295f3c81
ale-villegas <106825168+ale-villegas@users.noreply.github.com>
2024-05-31 00:04:07 +0200

    arreglando falla react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { MoviesProvider } from "./context/MoviesContext.tsx";
 import { getMovies } from "./services/getMovies.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MoviesProvider fetchData={getMovies}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </MoviesProvider>
 );
